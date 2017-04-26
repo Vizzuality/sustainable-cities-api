0936e2641e6ef55a85ade9f813117215f011773f
rschumann <sebastian.schkudlara@gmail.com>
2017-04-26 13:33:24 +0200

    Add deploy files

---
+++ config/deploy.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# config valid only for current version of Capistrano
+lock '3.8.0'
+
+set :application, 'Cities'
+set :repo_url, 'git@github.com:Vizzuality/sustainable_cities.git'
+
+set :default_env, {
+  'PATH' => "/home/ubuntu/.rvm/gems/ruby-2.4.0/bin:/home/ubuntu/.rvm/bin:$PATH",
+  'RUBY_VERSION' => 'ruby-2.4.0',
+  'GEM_HOME'     => '/home/ubuntu/.rvm/gems/ruby-2.4.0',
+  'GEM_PATH'     => '/home/ubuntu/.rvm/gems/ruby-2.4.0',
+  'BUNDLE_PATH'  => '/home/ubuntu/.rvm/gems/ruby-2.4.0'
+}
+
+set :passenger_restart_with_touch, true
+
+set :rvm_type, :user
+set :rvm_ruby_version, '2.4.0'
+set :rvm_roles, [:app, :web, :db]
+
+set :keep_releases, 5
+
+set :linked_files, %w{.env}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+
+set :rvm_map_bins, fetch(:rvm_map_bins, []).push('rvmsudo')
+
+namespace :deploy do
+  after :finishing, 'deploy:cleanup'
+  after 'deploy:publishing', 'deploy:symlink:linked_files', 'deploy:symlink:linked_dirs', 'deploy:restart'
+end
