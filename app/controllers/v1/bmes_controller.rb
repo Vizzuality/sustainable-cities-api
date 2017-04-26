0ade34b95dfebc13c3b8326d218f989f038d9b3f
rschumann <sebastian.schkudlara@gmail.com>
2017-04-26 14:03:26 +0200

    Fixes for bme params

--- app/controllers/v1/bmes_controller.rb
+++ app/controllers/v1/bmes_controller.rb
@@ -49,7 +49,7 @@ module V1
       end
 
       def bme_params
-        params.require(:bme).permit(:name, :description, :enabling_ids, :category_ids, :project_ids)
+        params.require(:bme).permit(:name, :description, { enabling_ids: [] }, { category_ids: [] })
       end
   end
 end
