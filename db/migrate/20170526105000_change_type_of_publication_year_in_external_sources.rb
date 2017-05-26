df58189f2b47282ae7a4c1f127d67b3fd2c747fe
Tiago Santos <santos.tiago@gmail.com>
2017-05-26 13:14:16 +0200

    Bug fix to migration

--- db/migrate/20170526105000_change_type_of_publication_year_in_external_sources.rb
+++ db/migrate/20170526105000_change_type_of_publication_year_in_external_sources.rb
@@ -3,7 +3,7 @@ class ChangeTypeOfPublicationYearInExternalSources < ActiveRecord::Migration[5.1
     add_column :external_sources, :temp_publication_year, :integer
 
     ExternalSource.all.each do |es|
-      es.temp_publication_year = es.publication_year.year
+      es.temp_publication_year = es.publication_year.year if es.publication_year.present?
       es.save!
     end
 
