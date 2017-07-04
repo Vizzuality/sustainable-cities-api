305df0ab91abc75c2d91ae75ed52ae76fb752314
Ivan Birkman <ivan.yk14@gmail.com>
2017-07-04 17:12:33 +0200

    Remove source from imapct if its removed from project

--- app/models/attacheable_external_source.rb
+++ app/models/attacheable_external_source.rb
@@ -12,4 +12,13 @@
 class AttacheableExternalSource < ApplicationRecord
   belongs_to :external_source
   belongs_to :attacheable, polymorphic: true
+
+  before_destroy :remove_dependent
+
+  def remove_dependent
+    if attacheable_type == 'Project'
+      impacts_ids = attacheable.impacts.pluck(:id)
+      AttacheableExternalSource.where(attacheable: impacts_ids, external_source_id: 325, attacheable_type: 'Impact').each(&:destroy)
+    end
+  end
 end
