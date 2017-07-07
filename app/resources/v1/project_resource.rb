58d2c101d81ca851c85bdd8081bcb4f5a9edf5b4
Ivan Birkman <ivan.yk14@gmail.com>
2017-07-07 12:29:45 +0200

    Remove useless fields

--- app/resources/v1/project_resource.rb
+++ app/resources/v1/project_resource.rb
@@ -2,7 +2,7 @@ module V1
   class ProjectResource < JSONAPI::Resource
     caching
 
-    attributes :name, :situation, :solution, :category_id, :solution_id, :solution_slug, :solution_name, :country_id,
+    attributes :name, :situation, :solution, :category_id, :country_id,
                :operational_year, :project_type, :is_active, :is_featured,
                :deactivated_at, :publish_request, :published_at, :bme_tree
 
@@ -21,19 +21,6 @@ module V1
     has_many :documents
     has_many :external_sources
     has_many :comments
-
-    def solution_id
-      object.category.parent.id rescue nil
-    end
-
-    def solution_slug
-      object.category.parent.slug rescue nil
-    end
-
-    def solution_name
-      object.category.parent.name rescue nil
-    end
-
 
     def custom_links(_)
       { self: nil }
