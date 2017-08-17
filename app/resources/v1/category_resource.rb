32d788c911ac569a910ccaa19527ffa1f78813e1
Ivan Birkman <ivan.yk14@gmail.com>
2017-08-17 12:39:11 +0200

    Add private categories route

--- app/resources/v1/category_resource.rb
+++ app/resources/v1/category_resource.rb
@@ -5,12 +5,12 @@ module V1
 
     attributes :name, :slug, :description, :category_type, :label, :level, :children_bmes
 
-    has_one :parent
-    has_many   :children
+    has_one  :parent
+    has_many :children
     has_many :bmes
     has_many :projects
     has_many :enablings
-    filters :id, :name, :slug, :category_type, :label, :level
+    filters  :id, :name, :slug, :category_type, :label, :level
 
     def custom_links(_)
       { self: nil }
