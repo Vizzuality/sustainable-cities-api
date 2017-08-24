ff06a1c51fd3d8ec621fde8753a121f806af8166
Ivan Birkman <ivan.yk14@gmail.com>
2017-08-24 15:53:12 +0200

    Add bme id to business model bme resource

--- app/resources/v1/business_model_bme_resource.rb
+++ app/resources/v1/business_model_bme_resource.rb
@@ -1,5 +1,7 @@
 module V1
   class BusinessModelBmeResource < JSONAPI::Resource
+    attributes :bme_id
+
     has_one :comment, polymorphic: true, foreign_key: :id
     has_one :bme
     has_one :business_model
