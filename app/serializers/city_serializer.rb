7d175ce79040c0d0fbd440b9e14faf802465e5ac
Ivan Birkman <ivan.yk14@gmail.com>
2017-09-28 19:00:27 +0200

    Add create city photos through api

--- app/serializers/city_serializer.rb
+++ app/serializers/city_serializer.rb
@@ -19,6 +19,7 @@ class CitySerializer < ActiveModel::Serializer
   attributes :id, :name, :iso, :lat, :lng, :province, :is_featured, :country_name
 
   belongs_to :country, serializer: CountrySerializer
+  has_many :photos, serializer: PhotoSerializer
 
   def country_name
     object.country.name rescue nil
