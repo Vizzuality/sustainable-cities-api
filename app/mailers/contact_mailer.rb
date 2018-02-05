8be04123cc7f1103b887c7b43d70a9ce3dd06925
Tiago Santos <santos.tiago@gmail.com>
2018-02-05 14:27:24 +0000

    Changed the contact's email subject

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,7 +3,7 @@ class ContactMailer < AsyncMailer
   def contact_email(user_email)
     @email     = user_email
 
-    @subject = 'Requested link to change your password'
+    @subject = 'Requested link to contact'
 
     mail(to: @email, subject: @subject)
   end
